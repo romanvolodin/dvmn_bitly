0f0b744a5b28112a649515b6f747106c1fdb7167
Roman Volodin <roman.volodin@gmail.com>
2021-08-12 23:14:30 +0300

    Simplify code: move headers to a variable.

--- bitly.py
+++ bitly.py
@@ -8,33 +8,33 @@ from environs import Env
 BITLY_API_URL = "https://api-ssl.bitly.com/v4"
 
 
-def shorten_link(token, url):
+def shorten_link(headers, url):
     response = requests.post(
         f"{BITLY_API_URL}/shorten",
-        headers = {"Authorization": f"Bearer {token}"},
+        headers = headers,
         json = {"long_url": url},
     )
     response.raise_for_status()
     return response.json()["link"]
 
 
-def count_clicks(token, link):
+def count_clicks(headers, link):
     parsed_url = urlparse(link)
     bitlink = f"{parsed_url.netloc}{parsed_url.path}"
     response = requests.get(
         f"{BITLY_API_URL}/bitlinks/{bitlink}/clicks/summary",
-        headers = {"Authorization": f"Bearer {token}"},
+        headers = headers,
     )
     response.raise_for_status()
     return response.json()["total_clicks"]
 
 
-def is_bitlink(token, link):
+def is_bitlink(headers, link):
     parsed_url = urlparse(link)
     bitlink = f"{parsed_url.netloc}{parsed_url.path}"
     response = requests.get(
         f"{BITLY_API_URL}/bitlinks/{bitlink}",
-        headers = {"Authorization": f"Bearer {token}"},
+        headers = headers,
     )
     return response.ok
 
@@ -58,16 +58,17 @@ if __name__ == "__main__":
     env.read_env() 
 
     api_token = env.str("BITLY_TOKEN")
+    headers = {"Authorization": f"Bearer {api_token}"}
 
     args = parse_arguments()
     input_url = args.input_url
 
-    if is_bitlink(api_token, input_url):
-        print(count_clicks(api_token, input_url))
+    if is_bitlink(headers, input_url):
+        print(count_clicks(headers, input_url))
         exit()
 
     try:
-        short_link = shorten_link(api_token, input_url)
+        short_link = shorten_link(headers, input_url)
     except requests.exceptions.HTTPError as err:
         print(err)
         exit("Make sure the url is correct")
